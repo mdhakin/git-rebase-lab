0415c33f9e949e10edbbae8a711c9dc6ec823625
mdhakin <mdhakin@alaska.edu>
2026-07-19 15:09:44 -0700

    Add subtract function

--- include/merge.h
+++ include/merge.h
@@ -7,6 +7,7 @@ class Merge
     public:
         int addTwoNumbers(int A, int B);
         int multiplyTwoNumbers(int A, int B);
+        int subtractTwoNumbers(int A, int B);
         std::uint8_t getNumberOfCalculations() const;
     private:
         std::uint8_t calculationCount_{0};
